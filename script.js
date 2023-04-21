c460374a2e8539f624a3973027566fbbb9f04200
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-21 17:13:34 +0200

    add script for create the table

---
+++ script.js
@@ -0,0 +1,17 @@
+const divTabella = document.getElementById("div_tabella");
+const altezzaLunghezza = 10; // TODO: bisogna cambiarlo con il valore del selector;
+let stampFinale = "";
+
+function tabella(){
+    let nCasella = 0;
+    for(let i = 0; altezzaLunghezza > i; i++){
+        stampFinale += `<div class="riga">`;
+        for(let j = 0; altezzaLunghezza > j; j++){
+            nC = nCasella + j + 1;
+            stampFinale += `<span class="casella" id="casella${nC}" onclick="casella(${nC})">${nC}</span>`;
+        }
+        stampFinale += `</div>`;
+        nCasella += 10;
+    }
+    divTabella.innerHTML = stampFinale;
+}
